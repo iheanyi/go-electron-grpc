982652edabc19eda8ae14dde5ed23dde993e11ec
Iheanyi Ekechukwu <iekechukwu@gmail.com>
2017-01-22 21:37:01 -0500

    Add in database package and have a CreateTodo method.

---
+++ server/database/database_test.go
@@ -0,0 +1,52 @@
+package database
+
+import (
+	"github.com/boltdb/bolt"
+	pb "github.com/iheanyi/go-electron-grpc/demo"
+	"os"
+	"testing"
+)
+
+func TestCreateTodo(t *testing.T) {
+	db, teardown, err := setupTestDatabase()
+	if err != nil {
+		t.Errorf("Error creating test database: %v", err)
+	}
+	defer teardown()
+
+	store := NewStore(db)
+	mock := &pb.Todo{
+		Description: "Test this",
+		Done:        false,
+	}
+
+	output, err := store.CreateTodo(mock)
+	if err != nil {
+		t.Errorf("Error thrown by CreateTodo: %v", err)
+	}
+
+	if output.Description != mock.Description {
+		t.Errorf("Got %v, want %v", output.Description, mock.Description)
+	}
+
+	if output.Done != mock.Done {
+		t.Errorf("Got %v, want %v", output.Done, mock.Done)
+	}
+
+	if output.Id < 1 {
+		t.Errorf("Expected Todo.Id to not be zero.")
+	}
+}
+
+func setupTestDatabase() (*bolt.DB, func(), error) {
+	// Wipe the database before every single test.
+	os.Remove("test.db")
+	db, err := bolt.Open("test.db", 0600, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return db, func() {
+		db.Close()
+	}, nil
+}
